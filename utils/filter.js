060dde1e4285aeb9a9d5bd1637310cd22bb5ceaf
JavierG <corotour55@gmail.com>
2026-07-18 17:18:33 +0200

    fix: reemplazar detector de malas palabras por lista fija (sin NLP), eliminar is-bad-word

--- utils/filter.js
+++ utils/filter.js
@@ -1,27 +1,32 @@
-const { BadWordsFilter } = require('is-bad-word');
 const palabrasPersonalizadas = require('./palabras-prohibidas');
 
-const filter = new BadWordsFilter();
-filter.loadAllLanguages();
-filter.addWords(palabrasPersonalizadas, 'spanish');
+const listaCompleta = palabrasPersonalizadas.map(p => p.toLowerCase());
 
-function contieneMalasPalabras(...textos) {
-  return textos.some(t => t && typeof t === 'string' && filter.isProfane(t, ['english', 'spanish']));
+function contiene(textos) {
+  for (const t of textos) {
+    if (t && typeof t === 'string') {
+      const lower = t.toLowerCase();
+      for (const palabra of listaCompleta) {
+        const regex = new RegExp('\\b' + palabra.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b');
+        if (regex.test(lower)) return true;
+      }
+    }
+  }
+  return false;
 }
 
-function encontrarMalasPalabras(...textos) {
+function encontrar(textos) {
   const encontradas = new Set();
   for (const t of textos) {
     if (t && typeof t === 'string') {
-      const matches = filter.detect(t, ['english', 'spanish']);
-      matches.forEach(m => encontradas.add(m.word.toLowerCase()));
+      const lower = t.toLowerCase();
+      for (const palabra of listaCompleta) {
+        const regex = new RegExp('\\b' + palabra.replace(/[.*+?^${}()|[\]\\]/g, '\\$&') + '\\b');
+        if (regex.test(lower)) encontradas.add(palabra);
+      }
     }
   }
-  const resultado = [...encontradas];
-  if (resultado.length > 0) {
-    console.log('🔍 Malas palabras detectadas:', resultado);
-  }
-  return resultado;
+  return [...encontradas];
 }
 
-module.exports = { contieneMalasPalabras, encontrarMalasPalabras };
+module.exports = { contieneMalasPalabras: contiene, encontrarMalasPalabras: encontrar };
